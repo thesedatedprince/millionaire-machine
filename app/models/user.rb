f809270f9bcaad95d5915efff1d26a366bd4c368
olwend <olwend@gmail.com>
2017-02-28 18:14:32 +0000

    15 SetupAssociations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :incomes, dependent: :destroy
+  has_many :expenditures, dependent: :destroy
 end
