5f6fb0fa10c3cf788d38643099b2128f934bd22b
Matthew Perry <thesedatedprince@gmail.com>
2017-03-01 11:08:25 +0000

    Just needs output to screen

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -4,4 +4,15 @@ class IncomesController < ApplicationController
   def new
     @income = Income.new
   end
+
+  def create
+    @income = Income.create(income_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def income_params
+    params.require(:income).permit(:amount, :name, :date, :frequency, :actual)
+  end
 end
--- spec/features/income_spec.rb
+++ spec/features/income_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 feature 'Incomes' do
   context 'no incomes added' do
     scenario 'should display a link to add income' do
-      visit '/incomes/new'
+      visit '/'
 
-      click_link 'Add income'
-      fill_in 'amount', with: '100'
-      fill_in 'name', with: 'Salary'
-      fill_in 'date', with: '28-02-2017'
-      fill_in 'actual', with: 'true'
-      fill_in 'frequency', with: 'weekly'
+      click_link 'Add Income'
+      fill_in 'Amount', with: '100'
+      fill_in 'Name', with: 'Salary'
+      fill_in 'Date', with: '28-02-2017'
+      fill_in 'Actual', with: 'true'
+      fill_in 'Frequency', with: 'weekly'
       click_button 'Add Entry'
       expect(page).to have_content 'Salary'
       expect(current_path).to eq '/dashboard'
