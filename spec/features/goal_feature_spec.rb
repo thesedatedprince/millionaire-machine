0b23b1443dd7931cd7c4d3d3746c5b177cad66a2
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-03-03 12:54:13 +0000

    User cannot leave blank fields on forms

--- spec/features/goal_feature_spec.rb
+++ spec/features/goal_feature_spec.rb
@@ -15,8 +15,10 @@ feature 'goals' do
       sign_up
       visit '/dashboards'
       click_link 'Add Goal'
-      fill_in('Amount', with: 1500)
+      fill_in('Amount', with: '1500')
       fill_in('Name', with: 'ski trip')
+      fill_in 'Start date', with: '28-02-2017'
+      fill_in 'Target date', with: '28-02-2017'
       click_button('Submit goal')
       expect(page).to have_content 'ski trip'
       expect(page).not_to have_content 'No goals yet'
@@ -28,16 +30,57 @@ feature 'goals' do
       sign_up
       visit '/dashboards'
       click_link 'Add Goal'
-      fill_in('Amount', with: 1500)
+      fill_in('Amount', with: '1500')
       fill_in('Name', with: 'ski trip')
+      fill_in 'Start date', with: '28-02-2017'
+      fill_in 'Target date', with: '28-02-2017'
       click_button('Submit goal')
       click_link 'Add Goal'
-      fill_in('Amount', with: 1500)
+      fill_in('Amount', with: '1500')
       fill_in('Name', with: 'vespa')
+      fill_in 'Start date', with: '28-02-2017'
+      fill_in 'Target date', with: '28-02-2017'
       click_button('Submit goal')
       expect(page).to have_content "Only permitted to store one goal"
       expect(page).not_to have_content 'vespa'
     end
   end
 
+  context 'User can not leave field blank' do
+    scenario 'User cannot leave Name field blank' do
+      sign_up
+      click_link 'Add Goal'
+      fill_in 'Amount', with: '100'
+      fill_in 'Name', with: ''
+      fill_in 'Start date', with: '28-02-2017'
+      fill_in 'Target date', with: '28-02-2017'
+      click_button 'Submit goal'
+      expect(page).to_not have_content '28-02-2017'
+      expect(current_path).to eq '/dashboards'
+  end
+
+  scenario 'User cannot leave Amount field blank' do
+    sign_up
+    click_link 'Add Goal'
+    fill_in 'Amount', with: ''
+    fill_in 'Name', with: 'Holiday'
+    fill_in 'Start date', with: '28-02-2017'
+    fill_in 'Target date', with: '28-02-2017'
+    click_button 'Submit goal'
+    expect(page).to_not have_content 'Holiday'
+    expect(current_path).to eq '/dashboards'
+end
+
+scenario 'User cannot leave Date field blank' do
+  sign_up
+  click_link 'Add Goal'
+  fill_in 'Amount', with: '100'
+  fill_in 'Name', with: 'Holiday'
+  fill_in 'Start date', with: ''
+  fill_in 'Target date', with: ''
+  click_button 'Submit goal'
+  expect(page).to_not have_content 'Holiday'
+  expect(current_path).to eq '/dashboards'
+end
+end
 end
